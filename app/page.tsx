4f89b963f055ad4010c9543439ca0848ef6f0b51
Hussein <hussmohupe@gmail.com>
2025-12-24 11:27:24 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggole } from "@/components/ui/theme-toggole";
+
+export default function Home() {
+  return (
+   <div>
+      <h1>Hello World</h1>
+      <ThemeToggole />
+   </div>
+  );
+}
